dbc355df7ebe7d2f5b98e7bd03d36d73237dab49
Pablito <tortugafrontend@gmail.com>
2022-05-27 21:53:30 +0300

    Second commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
+
+function App() {
+  const [cartISShown, setCartisShown] = useState(false)
+
+  const showCartHandler = () => {
+    setCartisShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartisShown(false);
+  }
+
+  return (
+    <CartProvider>
+      {cartISShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
+      <main>
+        <Meals/>
+      </main>
+    </CartProvider>
+  )
+}
+
+export default App;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvaliableMeals.module.css'
+
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchMeals = async () => {
+      const response = await fetch('https://food-order-app-9b760-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+
+      if(!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for(let key in responseData){
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    
+      fetchMeals().catch(error => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+
+  }, [])
+
+    if (isLoading) {
+      return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+      )
+    }
+
+    if(httpError) {
+      return (
+        <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>      );
+    }
+
+    const mealsList = meals.map((meal) => (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        meal={meal}
+      />
+    )); 
+
+    return (
+        <section className={classes.meals}>
+          <Card>
+            <ul>{mealsList}</ul>
+          </Card>
+        </section>
+    )
+}
+
+export default AvailableMeals
